2d9da52b3eddfb0c1021e82225e9cd626a65f841
gusty2306k <gustavo.chaves@sagradoalcal.edu.ar>
2023-11-10 13:30:59 -0300

    todos los ataques pasados a diccionarios

--- Ataques.py
+++ Ataques.py
@@ -90,302 +90,135 @@ picoTaladro = {"tipo": 7, "danio": 80, "precision": 100, "prioridad": 0, "usos":
 
 Fantasma8 =  ["Puño sombra","Puño fuego","Bola sombra","Impresionar"]
 
-def puñoSombra():
-    tipo = 8
-    daño = 60
-    precision = 100
-    prioridad = 0
-    usos = 24
 
-def robaSombra():
-    tipo = 8
-    daño = 80
-    precision = 80
-    prioridad = 0
-    usos = 20
-    cura = 200
 
-def bolaSombra():
-    tipo = 8
-    daño = 80
-    precision = 100
-    prioridad = 0
-    usos = 20
-    menosDef = 20
+puñoSombra = {'tipo': 8, 'danio': 60, 'precision': 100, 'prioridad': 0, 'usos': 24}
 
-def impresionar():
-    tipo = 8
-    daño = 30
-    precision = 100
-    prioridad = 0
-    usos = 12
-    asustar = 20
+robaSombra = {'tipo': 8, 'danio': 80, 'precision': 80, 'prioridad': 0, 'usos': 20, 'cura': 200}
+
+bolaSombra = {'tipo': 8, 'danio': 80, 'precision': 100, 'prioridad': 0, 'usos': 20, 'menosDef': 20}
+
+impresionar = {'tipo': 8, 'danio': 30, 'precision': 100, 'prioridad': 0, 'usos': 12, 'asustar': 20}
+
+
 
 Planta9 = ["Llueve hojas","Látigo cepa","Drenadoras","Espora"]
 
-def llueveHojas():
-    tipo = 9
-    daño = 130
-    precision = 80
-    prioridad = 0
-    usos = 5
 
-def latigoCepa():
-    tipo = 9
-    daño = 45
-    precision = 100
-    prioridad = 0 
-    usos = 40
+
+llueveHojas = {'tipo': 9, 'danio': 130, 'precision': 80, 'prioridad': 0, 'usos': 5}
+
+latigoCepa = {'tipo': 9, 'danio': 45, 'precision': 100, 'prioridad': 0, 'usos': 40}
+
+drenadoras = {'tipo': 9, 'danio': 60, 'precision': 100, 'prioridad': 0, 'usos': 40}
+
+espora = {'tipo': 9, 'danio':10 , 'precision': 100, 'prioridad': 0, 'usos': 40,'cura': 100 }
+
+
 
 Tierra10 = ["Bofetón lodo","Bomba fango","Excavar","Terremoto"]
 
-def bofetonLodo():
-    tipo = 10
-    daño =  30
-    precision = 100
-    prioridad = 0
-    menosPrecision = 15
-    usos = 8
 
-def bombaFango():
-    tipo = 10
-    daño = 65
-    precision = 85
-    prioridad = 0
-    menosPrecision = 30
-    usos = 8
 
-def excavar():
-    tipo = 10
-    daño = 85
-    precision = 100
-    prioridad = 0
-    usos = 20
+bofetonLodo = {'tipo': 10, 'danio': 30, 'precision': 100, 'prioridad': 0, 'menosPrecision': 15, 'usos': 8}
 
-def terremoto():
-    tipo = 10
-    daño = 130
-    precision = 90
-    prioridad = 0
-    usos = 10
+bombaFango = {'tipo': 10, 'danio': 65, 'precision': 85, 'prioridad': 0, 'menosPrecision': 30, 'usos': 8}
+
+excavar = {'tipo': 10, 'danio': 85, 'precision': 100, 'prioridad': 0, 'usos': 20}
+
+terremoto = {'tipo': 10, 'danio': 130, 'precision': 90, 'prioridad': 0, 'usos': 10}
+
+
 
 Hielo11 =  ["Lanza glacial","Martillo hielo","Puño hielo","Bola hielo"]
 
-def lanzaGlacial():
-    tipo = 11
-    daño = 120
-    precision = 100
-    prioridad = 0
-    usos = 5
 
-def martilloHielo():
-    tipo = 11
-    daño = 100
-    precision = 90
-    prioridad = 0
-    usos = 16
 
-def puñoHielo():
-    tipo = 11
-    daño = 75
-    precision = 100
-    prioridad = 0
-    usos = 20
+lanzaGlacial = {'tipo': 11, 'danio': 120, 'precision': 100, 'prioridad': 0, 'usos': 5}
 
-def bolaHielo():
-    tipo = 11
-    daño = 40
-    precision = 100
-    prioridad = 0
-    usos = 10
-    congelar = 10
+martilloHielo = {'tipo': 11, 'danio': 100, 'precision': 90, 'prioridad': 0, 'usos': 16}
+
+puñoHielo = {'tipo': 11, 'danio': 75, 'precision': 100, 'prioridad': 0, 'usos': 20}
+
+bolaHielo = {'tipo': 11, 'danio': 40, 'precision': 100, 'prioridad': 0, 'usos': 10, 'congelar': 10}
+
+
 
 Normal12 = ["Arañazo","Ataque rápido","Agarre","Tambor"]
 
-def arañazo():
-    tipo = 12
-    daño = 40
-    precision = 100
-    prioridad = 0
-    usos = 20
+arañazo = {'tipo': 12, 'danio': 40, 'precision': 100, 'prioridad': 0, 'usos': 20}
 
-def ataqueRapido():
-    tipo = 12
-    daño = 40
-    precision = 100
-    prioridad = 1
-    usos = 30
+ataqueRapido = {'tipo': 12, 'danio': 40, 'precision': 100, 'prioridad': 1, 'usos': 30}
 
-def tambor():
-    tipo = 12
-    dañopropio = 0.5
-    prioridad = 0
-    usos = 8
-    aumento = 2
+tambor = {'tipo': 12, 'daniopropio': 0.5, 'prioridad': 0, 'usos': 8, 'aumento': 2}
 
-def agarre():
-    tipo = 12
-    daño = 100
-    precision = 100
-    prioridad = 0
-    usos = 20
+agarre = {'tipo': 12, 'danio': 100, 'precision': 100, 'prioridad': 0, 'usos': 20}
      
 
 
 Veneno13 = ["Búnker","Carga tóxica","Envenenar","Bomba ácida"]
 
-def bunker():
-    tipo = 13
-    daño = 0
-    precision = 100
-    dañorival = 0
-    prioridad = 1
-    usos = 8
 
-def cargaToxica():
-    tipo = 13
-    daño = 45
-    precision = 100
-    prioridad = 0
-    usos = 30
 
-def envenenar():
-    tipo = 13
-    daño = 20
-    aumentoDeDaño = 2
-    precision = 80
-    prioridad = 0
-    usos = 10
+bunker = {'tipo': 13, 'danio': 0, 'precision': 100, 'dañorival': 0, 'prioridad': 1, 'usos': 8}
 
-def bombaAcida():
-    tipo = 13
-    daño = 50
-    precision = 100
-    prioridad = 0
-    usos = 20
+cargaToxica = {'tipo': 13, 'danio': 45, 'precision': 100, 'prioridad': 0, 'usos': 30}
+
+envenenar = {'tipo': 13, 'danio': 20, 'aumentoDeDaño': 2, 'precision': 80, 'prioridad': 0, 'usos': 10}
+
+bombaAcida = {'tipo': 13, 'danio': 50, 'precision': 100, 'prioridad': 0, 'usos': 20}
+
+
 
 Psiquico14 = ["Agilidad","Bola neblina","Danza lunar","Psicoataque"]
 
-def agilidad():
-    tipo = 14
-    daño = 0
-    aumento = 2
-    prioridad = 0
-    precision = 100
-    usos = 4
 
-def bolaNeblina():
-    tipo = 14
-    daño = 120
-    precision = 80
-    prioridad = 0
-    usos = 8
 
-def danzaLunar():
-    tipo = 14
-    daño = 0
-    precision = 100
-    prioridad = 0
-    usos = 8
-    aumento = 1.5
+agilidad = {'tipo': 14, 'danio': 0, 'aumento': 2, 'prioridad': 0, 'precision': 100, 'usos': 4}
 
-def psicoAtaque():
-    tipo = 14
-    daño = 80
-    precision = 100
-    prioridad = 0 
-    usos = 20
+bolaNeblina = {'tipo': 14, 'danio': 120, 'precision': 80, 'prioridad': 0, 'usos': 8}
+
+danzaLunar = {'tipo': 14, 'danio': 0, 'precision': 100, 'prioridad': 0, 'usos': 8, 'aumento': 1.5}
+
+psicoAtaque = {'tipo': 14, 'danio': 80, 'precision': 100, 'prioridad': 0, 'usos': 20}
+
 
 Roca15 = ["Pedrada","Lanzarrocas","Tumba rocas","Roca veloz"]
 
-def pedrada():
-    tipo = 15
-    daño = 90
-    precision = 100
-    prioridad = 0
-    usos = 20
 
-def lanzaRocas():
-    tipo = 15
-    daño = 30
-    precision = 100
-    prioridad = 0
-    usos = 15
 
-def tumbaRocas():
-    tipo = 15
-    daño = 150
-    precision = 60
-    prioridad = 0
-    usos = 8
+pedrada = {'tipo': 15, 'danio': 90, 'precision': 100, 'prioridad': 0, 'usos': 20}
 
-def rocaVeloz():
-    tipo = 15
-    daño = 50
-    precision = 100
-    prioridad = 1
-    usos = 20
+lanzaRocas = {'tipo': 15, 'danio': 30, 'precision': 100, 'prioridad': 0, 'usos': 15}
+
+tumbaRocas = {'tipo': 15, 'danio': 150, 'precision': 60, 'prioridad': 0, 'usos': 8}
+
+rocaVeloz = {'tipo': 15, 'danio': 50, 'precision': 100, 'prioridad': 1, 'usos': 20}
+
+
 
 Acero16 = ["Giro bola","Puño bala","Cuerpo pesado","Garra metal"]
 
-def giroBola():
-    tipo = 16
-    daño = 80
-    precision = 100
-    prioridad = 0
-    usos = 15
 
-def puñoBala():
-    tipo = 16
-    daño = 100
-    precision = 100
-    prioridad = 0
-    usos = 10
 
-def cuerpoPesado():
-    tipo = 16
-    daño = 200
-    precision = 100
-    prioridad = 0
-    usos = 2
+giroBola = {'tipo': 16, 'danio': 80, 'precision': 100, 'prioridad': 0, 'usos': 15}
 
-def garraMetal():
-    tipo = 16
-    daño = 40
-    precision = 100
-    prioridad = 1
-    usos = 10
+puñoBala = {'tipo': 16, 'danio': 100, 'precision': 100, 'prioridad': 0, 'usos': 10}
+
+cuerpoPesado = {'tipo': 16, 'danio': 200, 'precision': 100, 'prioridad': 0, 'usos': 2}
+
+garraMetal = {'tipo': 16, 'danio': 40, 'precision': 100, 'prioridad': 1, 'usos': 10}
+
+
 
 Agua17 = ["Burbuja","Pistola de agua","Hidrobomba","Hidropulso"]
 
-def burbuja():
-    tipo = 17
-    daño = 0
-    precision = 100
-    prioridad = 0
-    usos = 4
-    aumento = 1.5
 
-def pistolaDeAgua():
-    tipo = 17
-    daño = 60
-    precision = 100
-    prioridad = 1
-    usos = 30
 
-def hidroBomba():
-    tipo = 17
-    daño = 100
-    precision = 100
-    prioridad = 0
-    usos = 20
+burbuja = {'tipo': 17, 'danio': 0, 'precision': 100, 'prioridad': 0, 'usos': 4, 'aumento': 1.5}
 
-def hidroPulso():
-    tipo = 17
-    daño = 150 
-    precision = 80
-    prioridad = 0
-    usos = 5
+pistolaDeAgua = {'tipo': 17, 'danio': 60, 'precision': 100, 'prioridad': 1, 'usos': 30}
 
-def calcularEfectividad():
-    
+hidroBomba = {'tipo': 17, 'danio': 100, 'precision': 100, 'prioridad': 0, 'usos': 20}
+
+hidroPulso = {'tipo': 17, 'danio': 150, 'precision': 80, 'prioridad': 0, 'usos': 5}
